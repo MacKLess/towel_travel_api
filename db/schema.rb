2ed43e75a250be15a5b0e22a7fbf8e273c091d69
LDMacKrell <ldmackrell@gmail.com>
2017-12-14 16:25:02 -0800

    Update api to add versioning.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212222658) do
+ActiveRecord::Schema.define(version: 20171214231151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20171212222658) do
     t.string "planet"
     t.string "locals"
     t.string "transportation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
+  factory :user do
+    name "MyString"
+    email "MyString"
+    password_digest "MyString"
+  end
   factory(:destination) do
     planet("Broop Kidron Thirteen")
     location("Ibiza")
